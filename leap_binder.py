2e53d5d98439162c125523636d314c35e02ed079
tensorleap <yotam@tensorleap.ai>
2023-12-04 13:58:48 +0200

    WIP

--- leap_binder.py
+++ leap_binder.py
@@ -1,9 +1,9 @@
 from typing import List, Union
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
+# from sklearn.model_selection import train_test_split
+# from tensorflow.keras.datasets import mnist
+# from tensorflow.keras.utils import to_categorical
 
 # Tensorleap imports
 from code_loader import leap_binder
@@ -12,6 +12,7 @@ from code_loader.contract.enums import Metric, DatasetMetadataType
 from code_loader.contract.visualizer_classes import LeapHorizontalBar
 from utils.datasets import create_dataloader
 from leapcfg.config import CONFIG
+import torch
 import yaml
 class Namespace:
     def __init__(self, **kwargs):
@@ -19,15 +20,15 @@ class Namespace:
 # Preprocess Function
 def preprocess_func() -> List[PreprocessResponse]:
     # Hyperparameters
-    with open(CONFIG['hyp']) as f:
+    with open(CONFIG['HYP']) as f:
         hyp = yaml.load(f, Loader=yaml.SafeLoader)  # load hyps
-    with open(CONFIG['data']) as f:
+    with open(CONFIG['DATA']) as f:
         data_dict = yaml.load(f, Loader=yaml.SafeLoader)
     train_path = data_dict['train']
     test_path = data_dict['val']
     train_dataloader, dataset = create_dataloader(train_path, CONFIG['IMGSZ'], 1, CONFIG['GS'],
                                             Namespace(single_cls=CONFIG['SINGLE_CLS']),
-                                            hyp=hyp, augment=True, cache=CONFIG['CACHE_IMAGES'], rect=CONFIG['RECT'],
+                                            hyp=hyp, augment=False, cache=CONFIG['CACHE_IMAGES'], rect=CONFIG['RECT'],
                                             rank=-1, world_size=1, workers=CONFIG['WORKERS'],
                                             image_weights=False, quad=CONFIG['QUAD'], prefix='train: ')
     test_dataloader, dataset = create_dataloader(test_path, CONFIG['IMGSZ_TEST'], 1, CONFIG['GS'],\
@@ -39,21 +40,29 @@ def preprocess_func() -> List[PreprocessResponse]:
     # Generate a PreprocessResponse for each data slice, to later be read by the encoders.
     # The length of each data slice is provided, along with the data dictionary.
     # In this example we pass `images` and `labels` that later are encoded into the inputs and outputs
-    # train = PreprocessResponse(length=len(train_X), data={'images': train_X, 'labels': train_Y})
-    # val = PreprocessResponse(length=len(val_X), data={'images': val_X, 'labels': val_Y})
-    # response = [train, val]
+    train = PreprocessResponse(length=len(train_dataloader), data={'dataset': train_dataloader.dataset})
+    val = PreprocessResponse(length=len(test_dataloader), data={'dataset': test_dataloader.dataset})
+    response = [train, val]
     return response
 
 # Input encoder fetches the image with the index `idx` from the `images` array set in
 # the PreprocessResponse data. Returns a numpy array containing the sample's image. 
 def input_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
-    return preprocess.data['images'][idx].astype('float32')
+    return preprocess.data['dataset'][idx][0].permute((1, 2, 0)).numpy().astype('float32')/255.
 
 # Ground truth encoder fetches the label with the index `idx` from the `labels` array set in
 # the PreprocessResponse's data. Returns a numpy array containing a hot vector label correlated with the sample.
-def gt_encoder(idx: int, preprocessing: PreprocessResponse) -> np.ndarray:
-    return preprocessing.data['labels'][idx].astype('float32')
+def gt_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    #TODO - problem - can't have dynamic GT shape?
+    #Remove none-zero gt before picking up
+    torch_gt = preprocess.data['dataset'][idx][1]
+    np_gt = np.ones((CONFIG['MAX_INSTANCE'], 6))
+    instances_count = torch_gt.shape[0]
+    np_gt[:instances_count, :] = torch_gt[:min(CONFIG['MAX_INSTANCE'], instances_count), :]
+    return np_gt
 
+def custom_loss(gt, pred):
+    torch_gt = torch.from_numpy(gt[gt[...,0] == 0, :])
 # Metadata functions allow to add extra data for a later use in analysis.
 # This metadata adds the int digit of each sample (not a hot vector).
 def metadata_label(idx: int, preprocess: PreprocessResponse) -> int:
--- leap_custom_test.py
+++ leap_custom_test.py
@@ -1,1 +1,4 @@
-def test_mold
+from leap_binder import preprocess_func
+
+res = preprocess_func()
+print(1)
